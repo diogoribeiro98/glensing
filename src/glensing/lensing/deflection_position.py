949fab27ac17bbfb79b010eeec3279033cdbf30c
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-03-25 19:59:00 +0100

    deflection position function based on 3d vector class

---
+++ src/glensing/lensing/deflection_position.py
@@ -0,0 +1,73 @@
+import numpy as np
+from ..vector import vector, norm
+from ..physical_units import units
+
+from .einstein_radius import einstein_radius
+
+import matplotlib.pyplot as plt
+
+def deflection_position(sources             : vector, 
+						lens                : vector,
+						mass_lens           : float  = units.Mbh,
+						observer_distance   : float  = units.R0):
+
+	# Ensure input is iterable
+	if not isinstance(sources, list):
+		sources = [sources]  
+
+	p_images = []
+	s_images = []
+	e_radius = []
+
+
+	#Observer vector
+	observer = vector([0,0,observer_distance])
+ 	
+	for source in sources:
+		
+		# Check if lens is closer to the observer
+		rl = lens - observer
+		rs = source - observer
+
+		Dl = rl.norm()
+		Ds = rs.norm()
+
+		#If in front of lensing plane, no lensing happens
+		if Ds < Dl:
+
+			#No lensing
+			sky_pos_source = np.array([-rs.x/rs.z, -rs.y/rs.z]) * units.rad_to_as * 1e3
+			sky_pos_lens   = np.array([-rl.x/rl.z, -rl.y/rl.z]) * units.rad_to_as * 1e3
+			
+			p_images.append(sky_pos_source)
+			s_images.append(sky_pos_source)
+			e_radius.append(0.0)
+			
+			continue
+
+		# Get sky positions of source and lens
+		sky_pos_source = np.array([-rs.x/rs.z, -rs.y/rs.z]) * units.rad_to_as * 1e3
+		sky_pos_lens   = np.array([-rl.x/rl.z, -rl.y/rl.z]) * units.rad_to_as * 1e3
+
+		# Calculate angular distance vector
+		bvec = sky_pos_source - sky_pos_lens
+		beta = np.sqrt(np.sum(np.square(bvec)))
+		n_vector = bvec / beta
+
+		# Calculate deflection
+		Rg = units.G * mass_lens / units.c**2
+		Etheta = einstein_radius(Dl, (Ds - Dl), Rg=Rg, units='mas')
+		
+		Delta = np.sqrt(beta**2 + 4 * Etheta**2)
+
+		theta_plus = 0.5 * (beta + Delta)
+		theta_minus = 0.5 * (beta - Delta)
+
+		p_image = sky_pos_lens + n_vector * theta_plus
+		s_image = sky_pos_lens + n_vector * theta_minus
+
+		p_images.append(p_image)
+		s_images.append(s_image)
+		e_radius.append(Etheta)
+
+	return np.array(p_images), np.array(s_images), np.array(e_radius)
---
+++ src/glensing/lensing/einstein_radius.py
@@ -0,0 +1,33 @@
+import numpy as np
+from ..physical_units import units
+
+def einstein_radius(
+		D_l,
+		D_ls,
+		Rg = units.Rg,
+		units = 'mas'):
+	"""Returns the value of the einstein radius in the specified units
+
+	Args:
+		D_l (_float_): Distance from observer to lens in meters
+		D_ls (_float_): Distance from lens to emitter in meters
+		Rg (_float_, optional): Gravvitational radius of lens in meters. Defaults to units.Rg.
+		units(_str_,optional): Units in which to return the radius. Defaults to mas.
+	Returns:
+		_float_: Einstein radius in mas
+	"""
+	
+
+	if units == 'mas':
+		return np.sqrt( (D_ls/(D_l*(D_l+D_ls))) * 4*Rg  )*(180/np.pi)*3600*1e3
+	elif units == 'deg':
+		return np.sqrt( (D_ls/(D_l*(D_l+D_ls))) * 4*Rg  )*(180/np.pi)
+	elif units == 'as':
+		return np.sqrt( (D_ls/(D_l*(D_l+D_ls))) * 4*Rg  )*(180/np.pi)*3600
+	elif units == 'microas':
+		return np.sqrt( (D_ls/(D_l*(D_l+D_ls))) * 4*Rg  )*(180/np.pi)*3600*1e6
+	else:
+		raise KeyError(
+			f"{units} is not a valid argument units. It must be in ['deg','as','mas','microas']"
+		)
+	
